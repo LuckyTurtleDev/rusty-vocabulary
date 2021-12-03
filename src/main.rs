2b3529cf3e733ae39e9b20691239eee449520465
Lukas1818 <git@lukas1818.de>
2021-12-03 21:12:05 +0100

    change window from enum to struct

--- src/main.rs
+++ src/main.rs
@@ -33,24 +33,37 @@ struct WinMainMenu {
 	button_quit: button::State,
 }
 
-enum Window {
-	MainMenu(WinMainMenu),
-	AddVocabulary(WinAddVocabulary),
+enum State {
+	MainMenu,
+	AddVocabulary,
+}
+
+struct Window {
+	state: State,
+	main_menu: WinMainMenu,
+	add_vocabulary: WinAddVocabulary,
 }
 
 impl Sandbox for Window {
 	type Message = Message;
 
 	fn new() -> Window {
-		Window::MainMenu(WinMainMenu {
-			total_vocabulary: 0,
-			outstanding_vocabulary: 0,
-			subjects: 0,
-			button_querry: button::State::new(),
-			button_add: button::State::new(),
-			button_explore: button::State::new(),
-			button_quit: button::State::new(),
-		})
+		Window {
+			state: State::MainMenu,
+			main_menu: WinMainMenu {
+				total_vocabulary: 0,
+				outstanding_vocabulary: 0,
+				subjects: 0,
+				button_querry: button::State::new(),
+				button_add: button::State::new(),
+				button_explore: button::State::new(),
+				button_quit: button::State::new(),
+			},
+			add_vocabulary: WinAddVocabulary {
+				button_add: button::State::new(),
+				checkbox_both_sides: true,
+			},
+		}
 	}
 
 	fn title(&self) -> String {
@@ -61,60 +74,57 @@ impl Sandbox for Window {
 		match message {
 			Message::MainMenu(msg) => match msg {
 				MsgMainMenu::Quit => exit(0),
-				MsgMainMenu::Add => {
-					*self = Window::AddVocabulary(WinAddVocabulary {
-						button_add: button::State::new(),
-						checkbox_both_sides: true,
-					})
-				},
+				MsgMainMenu::Add => self.state = State::AddVocabulary,
 			},
 			Message::AddVocabulary(msg) => match msg {
-				MsgAddVocabulary::CheckboxBothSidesToogle => match self {
-					Window::AddVocabulary(state) => state.checkbox_both_sides = !state.checkbox_both_sides,
-					_ => panic!("Application is at the wrong state for this event"),
+				MsgAddVocabulary::CheckboxBothSidesToogle => {
+					self.add_vocabulary.checkbox_both_sides = !self.add_vocabulary.checkbox_both_sides
 				},
 			},
 		};
 	}
 
 	fn view(&mut self) -> Element<Self::Message> {
-		match self {
-			Window::MainMenu(state) => Row::new()
+		match self.state {
+			State::MainMenu => Row::new()
 				.push(Space::new(Length::Fill, Length::Shrink))
 				.push(
 					Column::new()
 						.push(Space::new(Length::Shrink, Length::Fill))
-						.push(Text::new(format!("total vocabulary: {}", state.total_vocabulary))) //todo: use //align_items(Alignment::Fill)
-						.push(Text::new(format!("outstanding vocabulary: {}", state.outstanding_vocabulary)))
-						.push(Text::new(format!("subjects/languages: {}", state.subjects)))
+						.push(Text::new(format!("total vocabulary: {}", self.main_menu.total_vocabulary))) //todo: use //align_items(Alignment::Fill)
+						.push(Text::new(format!("outstanding vocabulary: {}", self.main_menu.outstanding_vocabulary)))
+						.push(Text::new(format!("subjects/languages: {}", self.main_menu.subjects)))
 						.push(Space::new(Length::Shrink, Length::Fill)),
 				)
 				.push(Space::new(iced::Length::Units(20), Length::Shrink))
 				.push(
 					Column::new()
 						.push(Space::new(Length::Shrink, Length::Fill))
-						.push(Button::new(&mut state.button_querry, Text::new("querry vocabulary")))
+						.push(Button::new(&mut self.main_menu.button_querry, Text::new("querry vocabulary")))
 						.push(
-							Button::new(&mut state.button_add, Text::new("add vocabulary"))
+							Button::new(&mut self.main_menu.button_add, Text::new("add vocabulary"))
 								.on_press(Message::MainMenu(MsgMainMenu::Add)),
 						)
-						.push(Button::new(&mut state.button_explore, Text::new("explore vocabulary")))
+						.push(Button::new(
+							&mut self.main_menu.button_explore,
+							Text::new("explore vocabulary"),
+						))
 						.push(
-							Button::new(&mut state.button_quit, Text::new("quit"))
+							Button::new(&mut self.main_menu.button_quit, Text::new("quit"))
 								.on_press(Message::MainMenu(MsgMainMenu::Quit)),
 						)
 						.push(Space::new(Length::Shrink, Length::Fill)),
 				)
 				.push(Space::new(Length::Fill, Length::Shrink))
 				.into(),
-			Window::AddVocabulary(state) => Column::new()
+			State::AddVocabulary => Column::new()
 				.push(
 					Row::new()
 						.push(Text::new("subject/language"))
-						.push(Checkbox::new(state.checkbox_both_sides, "bot sides", |_| {
+						.push(Checkbox::new(self.add_vocabulary.checkbox_both_sides, "bot sides", |_| {
 							Message::AddVocabulary(MsgAddVocabulary::CheckboxBothSidesToogle)
 						}))
-						.push(Button::new(&mut state.button_add, Text::new("add vocabulary"))),
+						.push(Button::new(&mut self.add_vocabulary.button_add, Text::new("add vocabulary"))),
 				)
 				.into(),
 		}
