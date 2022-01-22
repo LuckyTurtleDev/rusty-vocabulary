b4896c2d047d8e2e4df2ead319aafa7c4679f783
Lukas1818 <git@lukas1818.de>
2022-01-22 20:09:37 +0100

    add check, if server is running

--- client/src/api.rs
+++ client/src/api.rs
@@ -1,8 +1,27 @@
-use anyhow::Result;
+use anyhow::{bail, Context};
 use attohttpc::{Method, RequestBuilder};
 use rusty_vocabulary_models::*;
 
-pub fn login(server: &str, username: &str, password: &str) -> Result<Token> {
+pub fn check_server(server: &str) -> anyhow::Result<()> {
+	let answer = RequestBuilder::try_new(Method::GET, format!("{server}/info"))?.send()?;
+	let info: Result<Info, attohttpc::Error> = answer.json();
+	if match info {
+		Err(_) => true,
+		Ok(info) => {
+			if info.about != "rusty-vocabulary" {
+				true
+			} else {
+				false
+			}
+		},
+	} {
+		bail!("Rusty-vocabulary is not running at this server");
+	}
+	Ok(())
+}
+
+pub fn login(server: &str, username: &str, password: &str) -> anyhow::Result<Token> {
+	check_server(&server).with_context(|| format!("Failed to connect to Server: "))?;
 	let answer: Token = RequestBuilder::try_new(Method::POST, format!("{server}/login"))?
 		.json(&Login {
 			username: username.into(),
