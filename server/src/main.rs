92763b37f1ff6ac42eb01df4e0bc4a2077cebb34
Lukas1818 <git@lukas1818.de>
2022-01-30 19:50:35 +0100

    code refactoring server

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,15 +3,14 @@ extern crate log;
 
 use crate::gotham::pipeline::{new_pipeline, single_pipeline};
 use gotham_restful::{
-	create,
 	gotham::{self, router::build_router},
-	read_all, AuthMiddleware, AuthResult, AuthSource, AuthStatus, AuthValidation, DrawResources, Raw, Resource,
-	ResourceError, StaticAuthHandler, Success,
+	AuthMiddleware, AuthSource, AuthValidation, DrawResources, StaticAuthHandler,
 };
-use jsonwebtoken::{encode, EncodingKey, Header};
 use rusty_vocabulary_models::*;
 use simple_logger::SimpleLogger;
-use std::time::{SystemTime, UNIX_EPOCH};
+
+mod resource;
+use resource::{card::*, info::*, login::*, status::*};
 
 const KEY: &[u8] = b"zlBsA2QXnkmpe0QTh8uCvtAEa4j33YAc";
 const EXP: u64 = 63115200; // 2 years
@@ -32,88 +31,8 @@ fn main() {
 			route.resource::<InfoResource>("info");
 			route.resource::<LoginResource>("login");
 			route.resource::<StatusResource>("status");
-			route.resource::<StatusResource>("card");
+			route.resource::<CardResource>("card");
 		}),
 	)
 	.unwrap();
-}
-
-#[derive(Resource)]
-#[resource(login)]
-struct LoginResource;
-
-#[derive(Debug, ResourceError)]
-pub enum LoginError {
-	#[status(INTERNAL_SERVER_ERROR)]
-	#[display("Internal Server Error")]
-	InternalServerError,
-}
-
-#[create]
-fn login(login: Login) -> Result<Raw<String>, LoginError> {
-	debug!("user {:?} has logged in", login.username);
-	warn!("login is still unimplemented");
-	let time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
-	let claims = Token {
-		iat: time,
-		exp: time + EXP,
-		user_name: login.username.clone(),
-	};
-	let token = encode(&Header::default(), &claims, &EncodingKey::from_secret(KEY));
-	match token {
-		Err(error) => {
-			error!("failed to generate login token for user {:?} : {}", login.username, error);
-			Err(LoginError::InternalServerError)
-		},
-		Ok(token) => Ok(Raw {
-			raw: token,
-			mime: mime::TEXT_PLAIN,
-		}),
-	}
-}
-
-#[derive(Resource)]
-#[resource(info)]
-struct InfoResource;
-
-#[read_all]
-fn info() -> Success<Info> {
-	Info {
-		version: env!("CARGO_PKG_VERSION").to_owned(),
-		about: "rusty-vocabulary".to_owned(),
-	}
-	.into()
-}
-
-#[derive(Resource)]
-#[resource(status)]
-struct StatusResource;
-
-#[read_all]
-fn status(auth: AuthStatus<Token>) -> AuthResult<Status, anyhow::Error> {
-	warn!("status is still unimplemented");
-	let token = auth.ok()?;
-	let status = Status {
-		vocabulary: 42,
-		outstanding_vocabulary: 42,
-		subjects: 42,
-		outstanding_subjects: 42,
-	};
-	Ok(status)
-}
-
-#[derive(Resource)]
-#[resource(card)]
-struct CardResource;
-
-#[read_all]
-fn card(auth: AuthStatus<Token>) -> AuthResult<Vec<CardPartial>, anyhow::Error> {
-	warn!("card is still unimplemented");
-	let token = auth.ok()?;
-	let card = CardPartial {
-		question: "This is a question?".into(),
-		answer: "and this is the answer".into(),
-		id: 42,
-	};
-	Ok(vec![card])
 }
