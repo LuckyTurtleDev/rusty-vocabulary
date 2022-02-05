4ad4fb97300322ed5651f288f41811fd2b44e411
Lukas1818 <git@lukas1818.de>
2022-02-05 19:28:50 +0100

    update crates

--- server/src/resource/card.rs
+++ server/src/resource/card.rs
@@ -1,18 +1,20 @@
 use gotham_restful::{read_all, AuthResult, AuthStatus, Resource};
-use rusty_vocabulary_models::*;
+use rusty_vocabulary_models::{card, Token};
 
 #[derive(Resource)]
 #[resource(card)]
 pub struct CardResource;
 
 #[read_all]
-fn card(auth: AuthStatus<Token>) -> AuthResult<Vec<CardPartial>, anyhow::Error> {
+fn card(auth: AuthStatus<Token>) -> AuthResult<Vec<card::Small>, anyhow::Error> {
 	warn!("card is still unimplemented");
 	let token = auth.ok()?;
-	let card = CardPartial {
-		question: "This is a question?".into(),
-		answer: "and this is the answer".into(),
+	let card = card::Small {
 		id: 42,
+		content: card::Content {
+			question: "This is a question?".into(),
+			answer: "and this is the answer".into(),
+		},
 	};
 	Ok(vec![card])
 }
